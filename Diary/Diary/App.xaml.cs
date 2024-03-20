94b90e750149f52bdc5f780570d1f3bd86b1eb9b
Filip Vágner <therazix@gmail.com>
2024-03-20 13:44:08 +0100

    feat: initialize project

---
+++ Diary/Diary/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Diary
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Diary/Diary/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Diary
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
