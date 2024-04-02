4f45527a41486dabdebff3f4486a05455bdfc3b2
Jana Stopková <jana.stopkova@intecs.cz>
2024-04-02 17:11:28 +0200

    feat: add entities

---
+++ Diary/Diary/Entities/EntityBase.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Diary.Entities
+{
+    public class EntityBase
+    {
+        [Key]
+        public Guid Id { get; set; }
+    }
+}
---
+++ Diary/Diary/Entities/Entry.cs
@@ -0,0 +1,25 @@
+﻿namespace Diary.Entities
+{
+    public class Entry : EntityBase
+    {
+        public string Title { get; set; } = string.Empty;
+
+        public string Content { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime EditedAt { get; set; }
+
+        public bool IsFavourite { get; set; }
+
+        public int Mood { get; set; }
+
+        public double Latitude { get; set; }
+
+        public double Longitude { get; set; }
+
+        public double Altitude { get; set; }
+
+        public virtual ICollection<Label> Labels { get; set; } = [];
+    }
+}
