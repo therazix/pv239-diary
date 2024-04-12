f5c23249cb452f90193deb53177f793d0ed0fdc0
Jana Stopková <jana.stopkova@intecs.cz>
2024-04-12 16:25:35 +0200

    feat: improve mood visualization in entry form

---
+++ Diary/Diary/Converters/IntToMoodColorConverter.cs
@@ -0,0 +1,25 @@
+﻿using CommunityToolkit.Maui.Converters;
+using System.Globalization;
+
+namespace Diary.Converters
+{
+    public class IntToMoodColorConverter : BaseConverterOneWay<int, Color>
+    {
+        private static readonly Color _defaultColor = Color.FromArgb("#FF56E74A");
+
+        public override Color ConvertFrom(int value, CultureInfo? culture)
+        {
+            return value switch
+            {
+                1 => Color.FromArgb("#FF56E74A"),
+                2 => Color.FromArgb("#FFD5F32B"),
+                3 => Color.FromArgb("#FFF1B502"),
+                4 => Color.FromArgb("#FFE06A00"),
+                5 => Color.FromArgb("#FFD91E1E"),
+                _ => _defaultColor,
+            };
+        }
+
+        public override Color DefaultConvertReturnValue { get; set; } = _defaultColor;
+    }
+}
---
+++ Diary/Diary/Converters/IntToMoodIconConverter.cs
@@ -0,0 +1,25 @@
+﻿using CommunityToolkit.Maui.Converters;
+using Diary.Resources.Fonts;
+using System.Globalization;
+
+namespace Diary.Converters;
+
+public class IntToMoodIconConverter : BaseConverterOneWay<int, string>
+{
+    private static readonly string _defaultMoodIconName = FontAwesomeIcons.SmilePlus;
+
+    public override string ConvertFrom(int value, CultureInfo? culture)
+    {
+        return value switch
+        {
+            1 => FontAwesomeIcons.LaughBeam,
+            2 => FontAwesomeIcons.Smile,
+            3 => FontAwesomeIcons.Meh,
+            4 => FontAwesomeIcons.Frown,
+            5 => FontAwesomeIcons.SadTear,
+            _ => _defaultMoodIconName,
+        };
+    }
+
+    public override string DefaultConvertReturnValue { get; set; } = _defaultMoodIconName;
+}
