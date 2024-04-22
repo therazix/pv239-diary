188e306324d042f67a2fa9b5e56cd748ea11a6c5
Jana Stopková <jana.stopkova@intecs.cz>
2024-04-22 16:20:11 +0200

    feat: template list modification

--- Diary/Diary/Models/Template/TemplateListModel.cs
+++ Diary/Diary/Models/Template/TemplateListModel.cs
@@ -5,5 +5,5 @@ public record TemplateListModel : ModelBase
 
     public string Name { get; set; } = string.Empty;
 
-    public string Content { get; set; } = string.Empty;
+    public int Mood { get; set; }
 }
