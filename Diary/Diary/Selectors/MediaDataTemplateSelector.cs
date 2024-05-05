79b790f24c553bfa7eecbf5845a229b86a68d26f
Filip Vágner <therazix@gmail.com>
2024-05-05 13:14:33 +0200

    refactor: set MediaDataTemplateSelector properties as nullable

--- Diary/Diary/Selectors/MediaDataTemplateSelector.cs
+++ Diary/Diary/Selectors/MediaDataTemplateSelector.cs
@@ -5,10 +5,10 @@ namespace Diary.Selectors;
 
 public class MediaDataTemplateSelector : DataTemplateSelector
 {
-    public DataTemplate ImageTemplate { get; set; }
-    public DataTemplate VideoTemplate { get; set; }
+    public DataTemplate? ImageTemplate { get; set; }
+    public DataTemplate? VideoTemplate { get; set; }
 
-    protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+    protected override DataTemplate? OnSelectTemplate(object item, BindableObject container)
     {
         if (item is MediaModel mediaModel)
         {
