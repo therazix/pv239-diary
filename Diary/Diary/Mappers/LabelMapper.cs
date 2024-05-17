56d420261bd82395ca53ddd6bdb8def51d599ce5
Filip Vágner <therazix@gmail.com>
2024-05-17 02:29:02 +0200

    feat: add color picker

--- Diary/Diary/Mappers/LabelMapper.cs
+++ Diary/Diary/Mappers/LabelMapper.cs
@@ -7,14 +7,26 @@ namespace Diary.Mappers;
 [Mapper]
 public static partial class LabelMapper
 {
+    [MapProperty(nameof(LabelEntity.Color), nameof(LabelDetailModel.Color), Use = nameof(MapColorStringToColor))]
     public static partial LabelDetailModel MapToDetailModel(this LabelEntity entity);
     public static partial ICollection<LabelDetailModel> MapToDetailModels(this ICollection<LabelEntity> entities);
 
+    [MapProperty(nameof(LabelEntity.Color), nameof(LabelListModel.Color), Use = nameof(MapColorStringToColor))]
     public static partial LabelListModel MapToListModel(this LabelEntity entity);
     public static partial ICollection<LabelListModel> MapToListModels(this ICollection<LabelEntity> entities);
 
+    [MapProperty(nameof(LabelListModel.Color), nameof(LabelEntity.Color), Use = nameof(MapColorToColorString))]
     [MapperIgnoreTarget(nameof(LabelEntity.Entries))]
     [MapperIgnoreTarget(nameof(LabelEntity.Templates))]
     public static partial LabelEntity MapToEntity(this LabelListModel model);
+
+    [MapProperty(nameof(LabelDetailModel.Color), nameof(LabelEntity.Color), Use = nameof(MapColorToColorString))]
     public static partial LabelEntity MapToEntity(this LabelDetailModel model);
+
+
+    [UserMapping(Default = false)]
+    private static string MapColorToColorString(Color color) => color.ToArgbHex(includeAlpha: true);
+
+    [UserMapping(Default = false)]
+    private static Color MapColorStringToColor(string color) => Color.FromArgb(color);
 }
